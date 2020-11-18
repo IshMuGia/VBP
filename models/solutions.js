e29bd82b7931805acf280782cead271f7b41ab9b
Mushira <Mushira.shaikh1999@gmail.com>
2020-11-18 15:19:59 +0530

    rate

--- models/solutions.js
+++ models/solutions.js
@@ -26,6 +26,11 @@ const SolSchema = new mongoose.Schema({
         type: String,
         required: true
 
+    },
+    logo: {
+        type: String,
+        required: true
+
     }
 });
 
--- routes/index.js
+++ routes/index.js
@@ -67,15 +67,6 @@ router.get("/charts", (req, res) => {
     });
 });
 
-// //res.redirect('/logged/?uid=' + req.query.uid);
-// router.get("/logged", (req, res) => {
-//     if (req.session.email) {
-//         res.redirect('/charts');
-//     } else {
-//         res.redirect("/myaccount")
-//     }
-// });
-
 router.get('/logout', (req, res) => {
     const currentDate = new Date();
     const lin = new Date(req.session.logdate);
@@ -101,116 +92,6 @@ router.get('/logout', (req, res) => {
                 }
                 res.redirect('/');
             });
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        });
-});
-
-router.get("/dproduct", (req, res) => {
-    const id = req.query.id;
-    console.log(id)
-    Prod.find({
-            model_no: id
-        })
-        .exec()
-        .then(docs1 => {
-            //console.log(docs1);
-            Prod.find({
-                    brand: docs1[0].brand
-                })
-                .exec()
-                .then(rel => {
-
-                    rel = rel.slice(0, 6);
-                    //console.log(rel.length);
-                    const docs = docs1.concat(rel)
-                    docs[7] = {
-                        "alert": "Save Product"
-                    };
-
-                    Rev.find({
-                            product: id
-                        })
-                        .exec()
-                        .then(docs2 => {
-                            //console.log("ksddhlkf");
-                            const document = docs.concat(docs2)
-                            //console.log(document)
-                            console.log(document.length);
-                            res.render('product', {
-                                results: document
-                            });
-                        })
-                        .catch(err => {
-                            res.status(500).json({
-                                error: err
-                            });
-                        });
-
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
-                });
-
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        });
-});
-
-// Category Shop
-router.get("/categoryshop", (req, res) => {
-
-    Prod.find({
-            category: req.query.category
-        })
-        .exec()
-        .then(results => {
-            console.log(results);
-            if (results) {
-                res.render("shop", {
-                    results: results
-                });
-            } else {
-                console.log("Empty")
-            }
-        })
-        .catch(err => console.log(err));
-});
-
-
-router.get("/addtocart", (req, res) => {
-    const email = req.query.email;
-    console.log(email)
-    Cart.find({
-            model_no: email
-        }, 'email model_no')
-        .exec()
-        .then(docs1 => {
-            Prod.find({
-                    product: id
-                })
-                .exec()
-                .then(docs2 => {
-                    const docs = docs1.concat(docs2)
-                    RT - AC86U
-                    console.log(docs)
-                    res.status(200).json({
-                        results: docs
-                    });
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
-                });
         })
         .catch(err => {
             res.status(500).json({
@@ -249,8 +130,8 @@ router.post("/addtowishlist", (req, res) => {
                 s = 1;
             }
             //console.log(s)
-            Prod.find({
-                    model_no: model_no
+            Sol.find({
+                    brand: brand
                 })
                 .exec()
                 .then(docs1 => {
@@ -315,7 +196,6 @@ router.post("/addtowishlist", (req, res) => {
 
 });
 
-
 router.get("/removefromwishlist", (req, res) => {
     var email = req.query.email;
     var model_no = req.query.model_no;
@@ -333,36 +213,15 @@ router.get("/removefromwishlist", (req, res) => {
         });
 });
 
-// Category Shop
-router.post("/categoryshop", (req, res) => {
-    Prod.find({
-            category: req.body.category
-        }, 'brand category sub_brand mrp _id')
-        .exec()
-        .then(results => {
-            console.log("lolvhgndk");
-            console.log(results);
-            if (results) {
-                res.render("shop", {
-                    results: results
-                });
-            } else {
-                console.log("Empty")
-            }
-        })
-        .catch(err => console.log(err));
-});
-
-//const g_docs = [];
 router.get("/wishlist", (req, res) => {
     var email = req.query.email;
     Wishlist.find({
             email: email
-        }, 'model_no -_id')
+        }, 'brand -_id')
         .exec()
         .then(docs1 => {
-            Prod.find()
-                .where('model_no')
+            Sol.find()
+                .where('brand')
                 .in(docs1.map(i => i.model_no))
                 .exec()
                 .then(records => {
@@ -385,42 +244,6 @@ router.get("/wishlist", (req, res) => {
         });
 });
 
-// Brand Shop
-router.get("/brandshop", (req, res) => {
-    Prod.find({
-            brand: req.query.brand
-        })
-        .then(results => {
-            if (results) {
-                console.log(results);
-                res.render("shop", {
-                    results: results
-                });
-            } else {
-                console.log("Empty")
-            }
-        })
-        .catch(err => console.log(err));
-});
-
-// SubBrand Shop
-router.get("/subbrandshop", (req, res) => {
-    Prod.find({
-            sub_brand: req.query.sub_brand
-        })
-        .then(results => {
-            if (results) {
-                console.log(results);
-                res.render("shop", {
-                    results: results
-                });
-            } else {
-                console.log("Empty")
-            }
-        })
-        .catch(err => console.log(err));
-});
-
 router.get("/shop", (req, res) => {
     // return res.sendFile("home.ejs", { root: path.join(__dirname, '/views') });
     res.render("shop");
@@ -436,24 +259,166 @@ router.get("/powerbi", (req, res) => {
     res.render("powerbi");
 });
 
-router.get("/checkout", (req, res) => {
-    // return res.sendFile("home.ejs", { root: path.join(__dirname, '/views') });
-    res.render("checkout");
-});
-
-router.get("/cart", (req, res) => {
-    // return res.sendFile("home.ejs", { root: path.join(__dirname, '/views') });
-    res.render("cart");
-});
 router.get("/wishlist", (req, res) => {
     // return res.sendFile("home.ejs", { root: path.join(__dirname, '/views') });
     res.render("wishlist");
 });
 
-router.get("/checkout", (req, res) => {
-    // return res.sendFile("home.ejs", { root: path.join(__dirname, '/views') });
-    res.render("checkout");
-});
+// Category Shop
+// router.get("/categoryshop", (req, res) => {
+
+//     Prod.find({
+//             category: req.query.category
+//         })
+//         .exec()
+//         .then(results => {
+//             console.log(results);
+//             if (results) {
+//                 res.render("shop", {
+//                     results: results
+//                 });
+//             } else {
+//                 console.log("Empty")
+//             }
+//         })
+//         .catch(err => console.log(err));
+// });
+
+// router.get("/dproduct", (req, res) => {
+//     const id = req.query.id;
+//     console.log(id)
+//     Prod.find({
+//             model_no: id
+//         })
+//         .exec()
+//         .then(docs1 => {
+//             //console.log(docs1);
+//             Prod.find({
+//                     brand: docs1[0].brand
+//                 })
+//                 .exec()
+//                 .then(rel => {
+
+//                     rel = rel.slice(0, 6);
+//                     //console.log(rel.length);
+//                     const docs = docs1.concat(rel)
+//                     docs[7] = {
+//                         "alert": "Save Product"
+//                     };
+
+//                     Rev.find({
+//                             product: id
+//                         })
+//                         .exec()
+//                         .then(docs2 => {
+//                             //console.log("ksddhlkf");
+//                             const document = docs.concat(docs2)
+//                             //console.log(document)
+//                             console.log(document.length);
+//                             res.render('product', {
+//                                 results: document
+//                             });
+//                         })
+//                         .catch(err => {
+//                             res.status(500).json({
+//                                 error: err
+//                             });
+//                         });
+
+//                 })
+//                 .catch(err => {
+//                     res.status(500).json({
+//                         error: err
+//                     });
+//                 });
+
+//         })
+//         .catch(err => {
+//             res.status(500).json({
+//                 error: err
+//             });
+//         });
+// });
+
+// Category Shop
+// router.post("/categoryshop", (req, res) => {
+//     Prod.find({
+//             category: req.body.category
+//         }, 'brand category sub_brand mrp _id')
+//         .exec()
+//         .then(results => {
+//             console.log("lolvhgndk");
+//             console.log(results);
+//             if (results) {
+//                 res.render("shop", {
+//                     results: results
+//                 });
+//             } else {
+//                 console.log("Empty")
+//             }
+//         })
+//         .catch(err => console.log(err));
+// });
+
+// router.get("/addtocart", (req, res) => {
+//     const email = req.query.email;
+//     console.log(email)
+//     Cart.find({
+//             model_no: email
+//         }, 'email model_no')
+//         .exec()
+//         .then(docs1 => {
+//             Prod.find({
+//                     product: id
+//                 })
+//                 .exec()
+//                 .then(docs2 => {
+//                     const docs = docs1.concat(docs2)
+//                     RT - AC86U
+//                     console.log(docs)
+//                     res.status(200).json({
+//                         results: docs
+//                     });
+//                 })
+//                 .catch(err => {
+//                     res.status(500).json({
+//                         error: err
+//                     });
+//                 });
+//         })
+//         .catch(err => {
+//             res.status(500).json({
+//                 error: err
+//             });
+//         });
+// });
+
+//res.redirect('/logged/?uid=' + req.query.uid);
+// router.get("/logged", (req, res) => {
+//     if (req.session.email) {
+//         res.redirect('/charts');
+//     } else {
+//         res.redirect("/myaccount")
+//     }
+// });
+
+// SubBrand Shop
+// router.get("/subbrandshop", (req, res) => {
+//     Prod.find({
+//             sub_brand: req.query.sub_brand
+//         })
+//         .then(results => {
+//             if (results) {
+//                 console.log(results);
+//                 res.render("shop", {
+//                     results: results
+//                 });
+//             } else {
+//                 console.log("Empty")
+//             }
+//         })
+//         .catch(err => console.log(err));
+// });
 
 // router.get("/logged", (req, res) => {
 //     if (req.session.email) {
