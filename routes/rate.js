6b25b3d7ad82980438c364c2e747c1eed2741da0
Mushira <Mushira.shaikh1999@gmail.com>
2020-11-19 18:09:33 +0530

    rate

--- routes/rate.js
+++ routes/rate.js
@@ -24,7 +24,7 @@ router.get("/", (req, res) => {
         .exec()
         .then(results => {
             console.log(results);
-            res.render('charts/?brand='+brand, {
+            res.redirect('Vcharts/?brand='+brand, {
                 results: results
             });
         })
